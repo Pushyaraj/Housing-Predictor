7f40d8e21a035858ac6b398037ef1b908eb7afac
Harsh <harsh9asnani@gmail.com>
2021-11-28 02:06:05 +0530

    main.py

--- main.py
+++ main.py
@@ -6,18 +6,21 @@ import seaborn as sb
 import sklearn as sk
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    data=pd.read_csv(r"C:\Users\harsh\Downloads\housing.csv")
-    prices=data['MEDV']
+    data=pd.read_csv(r"C:/Users/harsh/Downloads/california_housing.csv")
+    df=pd.DataFrame(data)
+    df=df.drop(columns=['latitude','longitude'])
+    prices=df['median_house_value']
     min_price=np.min(prices)
     max_price=np.max(prices)
     median_price=np.median(prices)
     mean_price=np.mean(prices)
     std_prices=np.std(prices)
-    #sb.pairplot(data,size=2.5)
+    #sb.pairplot(df,size=1)
     #plt.hist(data)
-
-    cm = np.corrcoef(data.T)
-    print(cm)
-    sb.heatmap(cm)
+    sb.scatterplot(x='total_rooms',y='median_house_value',data=df)
+    #cm = np.corrcoef(data.T)
+    print(df)
+    #print(type(data))
+    #sb.heatmap(cm)
     plt.show()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
