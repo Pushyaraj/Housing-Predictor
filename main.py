e485cf57e322d3e6312a59bba3314d7616e135b8
Harsh <harsh9asnani@gmail.com>
2021-11-29 06:35:36 +0530

    main.py

--- main.py
+++ main.py
@@ -2,9 +2,17 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sb
-
 import sklearn as sk
-# Press the green button in the gutter to run the script.
+
+def remove_outlier(df, column):
+    Q1=df.column.quantile(0.25)
+    Q3=df.column.quantile(0.75)
+    IQR=Q3-Q1
+    lower_limit=Q1-1.5*IQR
+    upper_limit=Q3+1.5*IQR
+    return "hello"
+
+
 if __name__ == '__main__':
     pd.set_option('display.max_columns', 10)
     data=pd.read_csv(r"C:/Users/harsh/Downloads/california_housing.csv")
@@ -16,13 +24,17 @@ if __name__ == '__main__':
     median_price=np.median(prices)
     mean_price=np.mean(prices)
     std_prices=np.std(prices)
-    #sb.pairplot(df,size=1)
+    #sb.pairplot(df,size=1,plot_kws={'line_kws':{'color':'red'},'scatter_kws':{'alpha':0.1}},kind="reg")
+    #sb.pairplot(df,hue='ocean_proximity')
+    correlation_housing=df.corr()
+    sb.heatmap(correlation_housing,annot=True)
     #plt.hist(data)
-    sb.histplot(df)
     #cm = np.corrcoef(data.T)
     #print(df)
     #print(type(data))
     #sb.heatmap(cm)
     print(df.describe())
     plt.show()
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
